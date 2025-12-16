22fb033e49f886eecd4528fd8ff24303b2445e39
/feofilov/IT-1/ <164215005+michaelgitjordan@users.noreply.github.com>
2025-12-17 01:17:16 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,243 +1,203 @@
-#include "Lab3.h"
+#include "TouristSet.h"
 #include <iostream>
-
+#include <vector>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 
-void showMainMenu() {
-    cout << endl;
-    cout << "-- Главное меню -- " << endl;
-    cout << "Выберите действие: " << endl;
-    cout << "1 -- Работа с классом Person " << endl;
-    cout << "2 -- Работа с классом Name " << endl;
-    cout << "3 -- Работа Department и Employee" << endl;
-    cout << "4 -- Работа с пистолетом " << endl;
-    cout << "5 -- Работа с автоматом " << endl;
-    cout << "6 -- Выход из проги " << endl;
-    cout << endl;
+const vector<string> POPULAR_COUNTRIES = {
+    "Франция", "Италия", "Испания", "Германия", "Великобритания",
+    "Греция", "Турция", "США", "Япония", "Таиланд",
+    "Мексика", "Португалия", "Нидерланды", "Швейцария", "Австрия"
+};
+
+void clearInput() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
 
-void personMenu() {
-    cout << "-- Person меню -- " << endl;
-    cout << "\nСоздание нового человека:" << endl;
-    Person customPerson;
-    customPerson.input();
-    cout << "Результат: ";
-    customPerson.print();
+bool isCountryValid(const string& country) {
+    return find(POPULAR_COUNTRIES.begin(), POPULAR_COUNTRIES.end(), country) 
+           != POPULAR_COUNTRIES.end();
 }
 
-void nameMenu() {
-    cout << "-- Name меню -- " << endl;
-    cout << "\nСоздание нового имени:" << endl;
-    Name customName;
-    customName.input();
-    cout << "Результат: ";
-    customName.print();   
-}
-
-void createNewPerson(Person& person) {
-    cout << "Создание нового человека " << endl;
-    person.input();
-    cout << endl;
-}
-
-// Старье
-void showCurrentPerson(const Person& customPerson){
-
-    if (customPerson.getName().empty()) {
-        cout << "Этот человек еще не 'создан' ! " << endl;
-    } else {
-        cout << "Текущий созданный человек: " << endl;
-        customPerson.print();
-        cout << endl;
-    }
-}
-
-void empDepExample() {
-    cout << "Пример работы сотрудников и отделов: " << endl;
-
-    Department itDepartment("IT");
-
-    Employee petrov("Петров", &itDepartment);
-    Employee kozlov("Козлов", &itDepartment);
-    Employee sidorov("Сидоров", &itDepartment);
-
-    // cout << "Назначаем сотрудников в отдел: " << endl;
-
-    // petrov.setDepartment(&itDepartment);
-    // kozlov.setDepartment(&itDepartment);
-    // sidorov.setDepartment(&itDepartment);
-
-    kozlov.setAsBoss();
-
-    cout << "Результат: " << endl;
-    petrov.print();
-    kozlov.print();
-    sidorov.print();
-
-    cout << "Инфа о отделе: " << endl;
-    itDepartment.print();
-
-    cout << "Коллеги Петрова:" << endl;
-    vector<Employee*> petrovColleagues = petrov.getDepartmentColleagues();
-    for (Employee* colleague : petrovColleagues) {
-        cout << "   - " << colleague->getName() << endl;
-    }
-
-    cout << endl;
-
-}
-
-void demPistol(){
-    cout << "-- Пистолет -- " << endl;
-
-    cout << "1. Создаем пистолет с 3 патронами:" << endl;
-    Pistol pistol(3);
-    pistol.print();
+void showCountries() {
+    cout << "\nСписок доступных стран: \n";
+    cout << "(нужно вводить точно как указано)\n\n";
     
-    cout << "\n2. Стреляем 5 раз:" << endl;
-    for (int i = 1; i <= 5; i++) {
-        cout << "Выстрел " << i << ": ";
-        pistol.shoot();
-    }
-
-}
-
-void automaticMenu() {
-    Automatic* automatic = nullptr;
-    int choice;
-    
-    do {
-        cout << "Автомат" << endl;
-        cout << "1 - Создать автомат по умолчанию (30/30)" << endl;
-        cout << "2 - Создать автомат с указанными патронами" << endl;
-        cout << "3 - Создать автомат с патронами и скорострельностью" << endl;
-        cout << "4 - Стрелять очередью" << endl;
-        cout << "5 - Стрелять N секунд" << endl;
-        cout << "6 - Перезарядить" << endl;
-        cout << "7 - Показать информацию" << endl;
-        cout << "8 - Назад в главное меню" << endl;
-        cout << "Выберите действие: ";
-        
-        cin >> choice;
-        cin.ignore();
-        
-        switch (choice) {
-            case 1:
-                if (automatic != nullptr) delete automatic;
-                automatic = new Automatic();
-                break;
-                
-            case 2: {
-                if (automatic != nullptr) delete automatic;
-                int bullets;
-                cout << "Введите количество патронов: ";
-                cin >> bullets;
-                cin.ignore();
-                automatic = new Automatic(bullets);
-                break;
-            }
-                
-            case 3: {
-                if (automatic != nullptr) delete automatic;
-                int bullets, rate;
-                cout << "Введите количество патронов: ";
-                cin >> bullets;
-                cout << "Введите скорострельность: ";
-                cin >> rate;
-                cin.ignore();
-                automatic = new Automatic(bullets, rate);
-                break;
-            }
-                
-            case 4:
-                if (automatic != nullptr) {
-                    automatic->shoot();
-                } else {
-                    cout << "Сначала создайте автомат!" << endl;
-                }
-                break;
-                
-            case 5:
-                if (automatic != nullptr) {
-                    int seconds;
-                    cout << "Введите количество секунд: ";
-                    cin >> seconds;
-                    cin.ignore();
-                    automatic->shootSeconds(seconds);
-                } else {
-                    cout << "Сначала создайте автомат!" << endl;
-                }
-                break;
-                
-            case 6:
-                if (automatic != nullptr) {
-                    int newBullets;
-                    cout << "Введите новое количество патронов: ";
-                    cin >> newBullets;
-                    cin.ignore();
-                    automatic->reload(newBullets);
-                } else {
-                    cout << "Сначала создайте автомат!" << endl;
-                }
-                break;
-                
-            case 7:
-                if (automatic != nullptr) {
-                    automatic->print();
-                } else {
-                    cout << "Сначала создайте автомат!" << endl;
-                }
-                break;
-                
-            case 8:
-                if (automatic != nullptr) {
-                    delete automatic;
-                    automatic = nullptr;
-                }
-                cout << "Возврат в главное меню..." << endl;
-                break;
-                
-            default:
-                cout << "Неверный выбор!" << endl;
+    for (size_t i = 0; i < POPULAR_COUNTRIES.size(); i++) {
+        cout << POPULAR_COUNTRIES[i];
+        if (i != POPULAR_COUNTRIES.size() - 1) {
+            cout << ", ";
         }
-    } while (choice != 8);
+        if ((i + 1) % 5 == 0) {
+            cout << endl;
+        }
+    }
+    cout << endl;
+}
+
+void inputForTourist(TouristSet& tourist, int touristNum) {
+    tourist.clear();
+    
+    cout << "\n Ввод для туриста " << touristNum << " \n";
+    showCountries();
+    
+    cout << "\nИнструкция:\n";
+    cout << "1. Вводите страны ТОЧНО как в списке выше\n";
+    cout << "2. Для завершения введите: stop\n";
+    cout << "3. Для просмотра списка введите: list\n\n";
+    
+    int count = 1;
+    while (true) {
+        cout << "Страна " << count << " (или 'stop'): ";
+        string input;
+        cin >> input;
+        
+        if (input == "stop") {
+            break;
+        }
+        
+        if (input == "list") {
+            showCountries();
+            continue;
+        }
+        
+        if (isCountryValid(input)) {
+            if (tourist.contains(input)) {
+                cout << "Страна '" << input << "' уже добавлена! Пропускаем.\n";
+            } else {
+                tourist.add(input);
+                cout << "Добавлено: " << input << "\n";
+                count++;
+            }
+        } else {
+            cout << "Ошибка: '" << input << "' нет в списке!\n";
+            cout << "Вводите страны точно как показано выше.\n";
+        }
+    }
+    
+    cout << "\nТурист " << touristNum << " посетил " 
+         << tourist.size() << " стран.\n";
+    cout << "================================\n";
 }
 
 int main() {
-    Person customPerson;
-    int choice;
-
-    do {
-        showMainMenu();
+    vector<TouristSet> tourists;
+    TouristSet allCountries(POPULAR_COUNTRIES);
+    
+    cout << "Посещение стран туристами: " << endl;
+    
+    while (true) {
+        cout << "\n Меню \n";
+        cout << "1. Ввести данные о туристах\n";
+        cout << "2. Посмотреть введенные данные\n";
+        cout << "3. Выполнить анализ\n";
+        cout << "4. Выйти\n";
+        cout << "Выберите: ";
+        
+        int choice;
         cin >> choice;
-        cout << endl;
-        cin.ignore();
-
-
-        switch(choice){
-            case 1:
-                personMenu();
-                break;
-            case 2:
-                nameMenu();
-                break;
-            case 3:
-                empDepExample();
-                break;
-            case 4:
-                demPistol();
-                break;
-            case 5:
-                automaticMenu();
-                break;
-            case 6:
-                cout << "До свидания! " << endl;
-                break;
-            default: 
-             cout << "Неверный выбор. Выбирайте от 1 до 5! " << endl;
+        clearInput();
+        
+        if (choice == 1) {
+            cout << "\nСколько туристов? ";
+            int n;
+            cin >> n;
+            clearInput();
+            
+            if (n <= 0) {
+                cout << "Неверное количество!\n";
+                continue;
+            }
+            
+            tourists.clear();
+            tourists.resize(n);
+            
+            for (int i = 0; i < n; i++) {
+                inputForTourist(tourists[i], i + 1);
+            }
+            
+            cout << "\nДанные сохранены!\n";
+            
+        } else if (choice == 2) {
+            if (tourists.empty()) {
+                cout << "\nНет данных! Сначала введите данные.\n";
+            } else {
+                cout << "\nВведенные данные: \n";
+                for (size_t i = 0; i < tourists.size(); i++) {
+                    cout << "Турист " << (i + 1) << ": ";
+                    if (tourists[i].isEmpty()) {
+                        cout << "нет стран";
+                    } else {
+                        auto countries = tourists[i].getElements();
+                        for (size_t j = 0; j < countries.size(); j++) {
+                            cout << countries[j];
+                            if (j != countries.size() - 1) {
+                                cout << ", ";
+                            }
+                        }
+                    }
+                    cout << endl;
+                }
+            }
+            
+        } else if (choice == 3) {
+            if (tourists.empty()) {
+                cout << "\nНет данных! Сначала введите данные.\n";
+                continue;
+            }
+            
+            cout << "Всего туристов: " << tourists.size() << endl;
+            cout << "Всего стран в перечне: " << allCountries.size() << endl;
+            
+            TouristSet visitedByAll = tourists[0];
+            for (size_t i = 1; i < tourists.size(); i++) {
+                visitedByAll = visitedByAll.Intersect(tourists[i]);
+            }
+            
+            cout << "\n1. Страны, которые посетили ВСЕ туристы:\n";
+            if (visitedByAll.isEmpty()) {
+                cout << "   (нет таких стран)\n";
+            } else {
+                auto countries = visitedByAll.getElements();
+                for (const auto& country : countries) {
+                    cout << "   - " << country << endl;
+                }
+            }
+            
+            TouristSet visitedBySome;
+            for (const auto& tourist : tourists) {
+                visitedBySome = visitedBySome.Union(tourist);
+            }
+            
+            cout << "\n2. Страны, которые посетили НЕКОТОРЫЕ туристы:\n";
+            auto someCountries = visitedBySome.getElements();
+            for (const auto& country : someCountries) {
+                cout << "   - " << country << endl;
+            }
+            
+            TouristSet visitedByNone = allCountries.Except(visitedBySome);
+            
+            cout << "\n3. Страны, которые НЕ посетил НИКТО из туристов:\n";
+            if (visitedByNone.isEmpty()) {
+                cout << "   (все страны кто-то посетил)\n";
+            } else {
+                auto noneCountries = visitedByNone.getElements();
+                for (const auto& country : noneCountries) {
+                    cout << "   - " << country << endl;
+                }
+            }
+                    
+        } else if (choice == 4) {
+            cout << "\nВыход из программы...\n";
+            break;
+            
+        } else {
+            cout << "\nНеверный выбор! Выберите 1-4.\n";
         }
-    } while (choice != 6);
-
+    }
+    
     return 0;
 }
